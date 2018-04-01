9589dae1e1985a93353277b6425f6b617ab46ebe
ashtneoi <ashtneoi@gmail.com>
2018-04-01 14:38:39 -0700

    Fix param name

--- src/bin/argcount.rs
+++ src/bin/argcount.rs
@@ -10,6 +10,6 @@ fn cmain(argc: isize, argv: *const *const u8) -> isize {
     go_rust(argc, argv, main);
 }
 
-fn main(argc: isize, _argv: *const *const u8, _env: *const *const u8) {
+fn main(argc: isize, _argv: *const *const u8, _envp: *const *const u8) {
     syscall::exit(argc);
 }
